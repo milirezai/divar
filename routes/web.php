0948a43fe13d86bfa2289444cb6a33b85aaed3ce
milirezai <miladrezaix@gmail.com>
2025-07-05 19:44:10 +0330

    system routing added

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ Route::get('create','HomeController@create','create');
 Route::post('store','HomeController@store','store');
 Route::get('edit/{id}','HomeController@edit','edit');
 Route::put('/update/{id}','HomeController@update','update');
-Route::delete('/delete/{id}','HomeController@destroy','delete');
+Route::delete('delete/{id}','HomeController@destroy','delete');
