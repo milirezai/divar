9d18aedb3a3568b25997efa753bad6dbbf4062c1
milirezai <miladrezaix@gmail.com>
2025-07-08 16:07:34 +0330

    connect database

--- system/Database/DBConnection/DBConnection.php
+++ system/Database/DBConnection/DBConnection.php
@@ -3,22 +3,39 @@ namespace System\Database\DBConnection;
 use PDO;
 use PDOException;
 
-class DBConnection
-{
-    public static $dbConnection=null;
-
-    private function __construct()
-    {}
-
-    public static function dbConnectionInstance()
+     class DBConnection
     {
-        if(self::$dbConnection==null)
+        private static $dbConnection=null;
+    
+        private function __construct()
+        {}
+    
+        public static function dbConnection()
         {
-             $dbConnectionInstance=new DBConnection();
-            self::$dbConnection=$dbConnectionInstance->DBConnection();
+            if(self::$dbConnection==null)
+            {
+                 $dbConnectionInstance=new DBConnection();
+                self::$dbConnection=$dbConnectionInstance->getDBConnection();
+            }
+            return self::$dbConnection;
         }
-        return DBConnection::$dbConnection;
+
+        private function getDBConnection()
+        {
+            $options=array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ);
+            try
+            {
+                return new PDO("mysql:host=".DBHOST.";dbname=".DBNAME,DBUSERNAME,DBPASSWORD,$options);
+            }
+            catch (PDOException $e)
+            {
+                echo $e->getMessage();
+                return false;
+            }
+        }
+        
+        public static function newInsertId()
+        {
+            return self::dbConnection()->lastInsertId();
+        }
     }
-    private function DBConnection()
-    {}
-}
