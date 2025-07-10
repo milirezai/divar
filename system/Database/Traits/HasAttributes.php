9cc9809540150115a42aa6b5ffd7f2c40e66e24e
milirezai <miladrezaix@gmail.com>
2025-07-10 16:15:48 +0330

    end-writing-trait- HasAttributes

--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -1,38 +1,76 @@
 <?php
-namespace System\Database\Traits
+namespace System\Database\Traits;
 
 trait  HasAttributes
  {
-     private function registerAttribute()
+    # registerAttribute
+     private function registerAttribute($object , string $attribute , $value)
      {
-
+         $this->inCastsAttributes($attribute) == true ? $object->$attribute = $this->castDecodeValue($attribute,$value) : $object->$attribute=$value;
      }
-     protected function arrayToAttribute()
+     # arrayToAttribute
+     protected function arrayToAttribute(array $array , $object = null)
      {
-
+         if (!$object)
+         {
+             $className=get_called_class();
+             $object=new $className;
+         }
+         foreach ($array as $attribute => $value)
+         {
+             if ($this->inHiddenAttributes($attribute))
+                 continue;
+             $this->registerAttribute($object,$attribute,$value);
+         }
+         return $object;
      }
-     protected function arrayToObjects()
+     # arrayToObjects
+     protected function arrayToObjects(array $array)
      {
-
+         $collection= [];
+         foreach ($array as $value)
+         {
+             $object=$this->arrayToAttribute($value);
+             array_push($collection,$object);
+         }
+         $this->collection=$collection;
      }
-     private function inHiddenAttributes()
+     # inHiddenAttributes
+     private function inHiddenAttributes($attribute)
      {
-
+         return in_array($attribute,$this->hidden);
      }
-     private function inCastsAttributes()
+     # inCastsAttributes
+     private function inCastsAttributes($attribute)
      {
-
+         return in_array($attribute,array_keys($this->casts));
      }
-     private function castDecodeValue()
+     # castDecodeValue
+     private function castDecodeValue($attributeKey , $value)
      {
-
+         if ($this->casts[$attributeKey] == 'array' or $this->casts[$attributeKey] == 'object')
+         {
+             return unserialize($value);
+         }
+         return $value;
      }
-     private function castEncodeValue()
+     # castEncodeValue
+     private function castEncodeValue($attributeKey , $value)
      {
-
+         if ($this->casts[$attributeKey] == 'array' or $this->casts[$attributeKey] == 'object')
+         {
+             return serialize($value);
+         }
+         return $value;
      }
-     private function arrayToCastEncodeValue()
+     # arrayToCastEncodeValue
+     private function arrayToCastEncodeValue($values)
      {
-
+         $newArray= [];
+         foreach ($values as $attribute => $value)
+         {
+             $this->inCastsAttributes($attribute) == true ? $newArray[$attribute] = $this->castEncodeValue($attribute,$value) : $newArray[$attribute] = $value ;
+         }
+         return $newArray;
      }
  }
