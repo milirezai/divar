327c3c6a28c6a42e2e098149e1d85c7dfd9859a6
milirezai <miladrezaix@gmail.com>
2025-07-12 15:33:03 +0330

    start-writing-trait- HasCRUD

--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -1,1 +1,20 @@
 <?php
+namespace System\Database\Traits;
+use System\Database\DBConnection\DBConnection;
+trait HasCRUD
+{
+    protected function fill()
+    {
+        $fileArray= [];
+        foreach ($this->fillable as $attribute)
+        {
+            if (isset($this->$attribute))
+            {
+                array_push($fileArray,$this->getAttributeName($attribute)."= ?");
+                $this->inCastsAttributes($attribute) == true ? $this->addValue($attribute,$this->castEncodeValue($attribute,$this->$attribute)) : $this->addValue($attribute,$this->$attribute);
+            }
+        }
+        $fileString=implode(",",$fileArray);
+        return $fileString;
+    }
+}
