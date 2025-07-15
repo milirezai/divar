764afa2410e7e272800a81131a3b6645bd0956f4
milirezai <miladrezaix@gmail.com>
2025-07-15 20:01:19 +0330

    start-writing-trait- HasRelation

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -1,2 +1,32 @@
 <?php
 
+namespace System\Database\Traits;
+trait HasRelation
+{
+    # hasOneMethod
+    protected function hasOne($model, $foreignKey, $localKey)
+    {
+
+        if ($this->{$this->primaryKey})
+        {
+            $modelObject= new $model;
+            return $modelObject->getHaseOneRelation($this->table, $foreignKey, $localKey, $this->$localKey);
+        }
+
+    }
+    # getHaseOneRelationMethod
+    public function getHaseOneRelation($table, $foreignKey, $otherKey, $otherKeyValue)
+    {
+        $this->setSql("SELECT `b`.* FROM `{$table}` AS `a` JOIN ".$this->getTableName()." AS `b` ON `a`.`{$otherKey}` = `b`.`{$foreignKey}` ");
+        $this->setWhere("AND", " `a`.`{$otherKey}` = ? ");
+        $this->table= 'b';
+        $this->addValue($otherKey,$otherKeyValue);
+        $statement=$this->executeQuery();
+        $data=$statement->fetchAll();
+        if ($data)
+        {
+            $this->arrayToAttributes($data);
+        }
+        return [];
+    }
+}
