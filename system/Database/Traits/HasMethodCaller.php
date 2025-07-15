93c51796e17b8318c3a9c4b3888617b88e34c036
milirezai <miladrezaix@gmail.com>
2025-07-15 15:49:52 +0330

    start-writing-trait- HasMethodCaller

--- system/Database/Traits/HasMethodCaller.php
+++ system/Database/Traits/HasMethodCaller.php
@@ -1,1 +1,13 @@
 <?php
+namespace System\Database\Traits;
+trait HasMethodCaller
+{
+    private $allMethods= ["create","update","delete","find","all","save","where","whereOr","whereIn","whereNull","whereNotNull","limit","orderBy","get","paginate"];
+    private $allowedMethods= ["create","update","delete","find","all","save","where","whereOr","whereIn","whereNull","whereNotNull","limit","orderBy","get","paginate"];
+
+    protected function setAllowedMethods($array)
+    {
+        $this->allowedMethods=$array;
+    }
+
+}
