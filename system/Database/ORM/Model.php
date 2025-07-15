4dc99f7f01663a3b7f7f09c7547b32c4fe12814b
milirezai <miladrezaix@gmail.com>
2025-07-15 17:28:31 +0330

    start-writing-trait- HasMethodCaller

--- system/Database/ORM/Model.php
+++ system/Database/ORM/Model.php
@@ -3,14 +3,13 @@ namespace System\Database\ORM;
 
 use System\Database\Traits\HasAttributes;
 use System\Database\Traits\HasCRUD;
-use System\Database\Traits\HasSoftDelete;
 use System\Database\Traits\HasRelation;
 use System\Database\Traits\HasQueryBuilder;
 use System\Database\Traits\HasMethodCaller;
  
  abstract class Model
  {
-     use HasAttributes,HasCRUD,HasSoftDelete,HasRelation,HasQueryBuilder,HasMethodCaller;
+     use HasAttributes,HasCRUD,HasRelation,HasQueryBuilder,HasMethodCaller;
      protected $table;
      protected $fillable= [];
      protected $hidden= [];
--- system/Database/Traits/HasSoftDelete.php
+++ system/Database/Traits/HasSoftDelete.php
@@ -1,1 +1,24 @@
 <?php
+namespace System\Database\Traits;
+
+trait HasSoftDelete
+{
+    # deleteMethod
+    protected function deleteMethod($id = null)
+    {
+        $object= $this;
+        if ($id)
+        {
+            $this->resetQuery();
+            $object=$this->findMethod($id);
+        }
+        if ($object)
+        {
+            $object->resetQuery();
+            $object->setSql("UPDATE ".$object->getTableName()." SET ".$object->getAttributeName($object->deletedAT)." = NOW()");
+            $object->setWhere("AND",$this->getAttributeName($object->primaryKey)."= ?");
+            $object->addValue($object->primaryKey,$object->{$object->primaryKey});
+            return $object->executeQuery();
+        }
+    }
+}
