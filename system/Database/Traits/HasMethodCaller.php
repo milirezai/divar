fc42f501d93d5d99b22dd7a6770308cb0b46eee7
milirezai <miladrezaix@gmail.com>
2025-07-15 16:50:59 +0330

    :

--- system/Database/Traits/HasMethodCaller.php
+++ system/Database/Traits/HasMethodCaller.php
@@ -5,6 +5,27 @@ trait HasMethodCaller
     private $allMethods= ["create","update","delete","find","all","save","where","whereOr","whereIn","whereNull","whereNotNull","limit","orderBy","get","paginate"];
     private $allowedMethods= ["create","update","delete","find","all","save","where","whereOr","whereIn","whereNull","whereNotNull","limit","orderBy","get","paginate"];
 
+    public function __call(string $method, array $args)
+    {
+        return $this->methodCaller($this, $method, $args);
+    }
+
+    public static function __callStatic(string $method, array $args)
+    {
+        $className= get_called_class();
+        $instance= new $className();
+        return $instance::methodCaller($instance, $method , $args);
+    }
+
+    private function methodCaller($object, $method, $args)
+    {
+        $suffix= "Method";
+        $methodName= $method.$suffix;
+        if (in_array($method,$this->allowedMethods))
+        {
+            return call_user_func_array(array($object,$methodName),$args);
+        }
+    }
     protected function setAllowedMethods($array)
     {
         $this->allowedMethods=$array;
