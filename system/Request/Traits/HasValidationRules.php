eef98639e94a98b5636ceb8a53c0f884783eab2e
milirezai <miladrezaix@gmail.com>
2025-07-21 14:41:14 +0330

    end-writing-trait-HasValidationRules

--- system/Request/Traits/HasValidationRules.php
+++ system/Request/Traits/HasValidationRules.php
@@ -78,4 +78,124 @@ trait HasValidationRules
 
     }
 
+    protected function maxStr($name , $count)
+    {
+
+        if ($this->checkFieldExist($name))
+        {
+            if (strlen($this->request[$name]) > $count and $this->checkFirstError($name))
+            {
+                $this->setError($name, "{$name} must not exceed {$count} characters!");
+            }
+        }
+
+    }
+
+    protected function minStr($name , $count)
+    {
+
+        if ($this->checkFieldExist($name))
+        {
+            if (strlen($this->request[$name]) < $count and $this->checkFirstError($name))
+            {
+                $this->setError($name, "{$name} must not be less than {$count} characters!");
+            }
+        }
+
+    }
+
+    protected function maxNumber($name , $count)
+    {
+
+        if ($this->checkFieldExist($name))
+        {
+            if ($this->request[$name] > $count and $this->checkFirstError($name))
+            {
+                $this->setError($name,"{$name} must not be less than {$count}!");
+            }
+        }
+
+    }
+
+    protected function minNumber($name , $count)
+    {
+
+        if ($this->checkFieldExist($name))
+        {
+            if ($this->request[$name] < $count and $this->checkFirstError($name))
+            {
+                $this->setError($name,"{$name} must not be more than {$count}!");
+            }
+        }
+
+    }
+
+    protected function required($name)
+    {
+
+        if ((!isset($this->request[$name]) || $this->request[$name] === '') and $this->checkFirstError($name))
+        {
+            $this->setError($name,"The {$name} field should be required!");
+        }
+
+    }
+
+    protected function number($name)
+    {
+
+        if ($this->checkFieldExist($name))
+        {
+            if (!is_numeric($this->request[$name]) and $this->checkFirstError($name))
+            {
+                $this->setError($name, "The {$name} format must be a number!");
+            }
+        }
+
+    }
+
+    protected function date($name)
+    {
+
+        if ($this->checkFieldExist($name))
+        {
+            if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$this->request[$name]))
+            {
+                $this->setError($name,"The {$name} format is not valid");
+            }
+        }
+
+    }
+
+    protected function email($name)
+    {
+        if($this->checkFieldExist($name))
+        {
+            if(!filter_var($this->request[$name], FILTER_VALIDATE_EMAIL) and $this->checkFirstError($name))
+            {
+                $this->setError($name,"The {$name} format is not valid");
+            }
+        }
+    }
+
+    public function existsIn($name, $table, $field= "id")
+    {
+
+        if($this->checkFieldExist($name))
+        {
+            if ($this->checkFirstError($name))
+            {
+                $value=$this->$name;
+                $sql= "SELECT COUNT(*) FORM $table WHERE $field = ?";
+                $statement= DBConnection::dbConnection()->prepare($sql);
+                $statement->execute([$value]);
+                $result= $statement->fetchColumn();
+                if ($result == 0 or $result === false)
+                {
+                    $this->setError($name,"The {$name} field  not exist.");
+                }
+            }
+        }
+
+    }
+
 }
