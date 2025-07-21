796e4a7846b54a3a38bae2d7bb5997560095aa88
milirezai <miladrezaix@gmail.com>
2025-07-21 16:57:39 +0330

    end-writing-trait-HasRunValidation

--- system/Request/Traits/HasRunValidation.php
+++ system/Request/Traits/HasRunValidation.php
@@ -27,4 +27,30 @@ trait HasRunValidation
 
     }
 
+    private function checkFieldExist($name)
+    {
+        return (isset($this->request[$name]) and !empty($this->request[$name])) ? true : false;
+    }
+
+    private function checkFileExist($name)
+    {
+
+        if (isset($this->files[$name]['name']))
+        {
+            if (!empty($this->files[$name]['name']))
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    private function setError($name, $errorMsg)
+    {
+        array_push($this->errorVariablesName, $name);
+        error($name,$errorMsg);
+        $this->errorExist = true;
+    }
+
 }
