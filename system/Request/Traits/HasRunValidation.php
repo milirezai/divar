d4334f809ea9ed2e0834615102febbe27b433749
milirezai <miladrezaix@gmail.com>
2025-07-21 16:20:57 +0330

    tart-writing-trait-HasRunValidation

---
+++ system/Request/Traits/HasRunValidation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace System\Request\Traits;
+
+trait HasRunValidation
+{
+
+    protected function errorRedirect()
+    {
+
+        if ($this->errorExist == false)
+        {
+            return $this->request;
+        }
+        return back();
+
+    }
+
+    private function checkFirstError($name)
+    {
+
+        if (!errorExist($name) and !in_array($name, $this->errorVariablesName))
+        {
+            return true;
+        }
+        return false;
+
+    }
+
+}
