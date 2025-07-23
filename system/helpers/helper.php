6c820a2bd3f45218397baae54d2578347ee73670
milirezai <miladrezaix@gmail.com>
2025-07-23 20:02:51 +0330

    Helper functions development

--- system/helpers/helper.php
+++ system/helpers/helper.php
@@ -1,1 +1,12 @@
 <?php
+
+function view($dir,$vars)
+{
+    $viewBuilder = new \System\View\ViewBuilder();
+    $viewBuilder->run($dir);
+    $viewVars = $viewBuilder ->vars;
+    $content = $viewBuilder->content;
+    empty($viewVars) ? :extract($viewVars);
+    empty($vars) ? :extract($vars);
+    eval("?>".html_entity_decode($content));
+}
